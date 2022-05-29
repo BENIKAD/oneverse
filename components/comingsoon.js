bf8318529319f4db9ce2b6c97ab99e3ed2b9e4e6
Ikuesan Benjmin <benikad94@gmail.com>
2022-05-29 14:07:13 +0100

    init

---
+++ components/comingsoon.js
@@ -0,0 +1,14 @@
+import React from "react";
+ 
+import ComingSoon from "react-coming-soon";
+ 
+export default function App() {
+  return (
+    <ComingSoon
+      image="https://upload.wikimedia.org/wikipedia/commons/0/01/Ethereum_logo_translucent.svg"
+      bgColor="#fff"
+      textColor="#000"
+      illustration="development"
+    />
+  );
+}
---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+    {
+      price: 17,
+      from: '0xQazi',
+      to: '0xDavid',
+      date: '5 hours ago',
+    },
+    {
+      price: 17,
+      from: '0xQazi',
+      to: '0xDavid',
+      date: '10 mins ago',
+    },
+    {
+      price: 17,
+      from: '0xQazi',
+      to: '0xDavid',
+      date: '4 hours ago',
+    },
+    {
+      price: 17,
+      from: '0xQazi',
+      to: '0xDavid',
+      date: '2 hours ago',
+    },
+  ]
